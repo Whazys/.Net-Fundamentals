3bbd8bd4a9e8b41ff8eb597fa6061a76ccca9c91
whazys <santos_rafa1973@hotmail.com>
2021-11-21 16:10:15 -0300

    .Net-Fundamentals - DIO

--- NetFundamentals/Program.cs
+++ NetFundamentals/Program.cs
@@ -1,8 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-
-int numeroDeVezes = 5;
-for (int i = 0; i < numeroDeVezes; i++)
+using System;
+namespace NetFundamentals.Exemplos
 {
-  Console.WriteLine($"Bora estudar, Rafa! {i}");
+  class Program
+  {
+    static void Main()
+    {
+      var s = new Pilha();
+      s.Empilha(1);
+      s.Empilha(10);
+      s.Empilha(100);
+      Console.WriteLine(s.Desempilha());
+      Console.WriteLine(s.Desempilha());
+      Console.WriteLine(s.Desempilha());
+    }
+  }
 }
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 -- BootCamp - DIO: .Net Fundamentals
 
-01 - Conceitos/Frameworks (teórica)
+01 - Conceitos/Frameworks (teórica).
 
 02 - Preparando ambiente e aplicação no console.
+
+03 - Introdução C#: Conceito & Estrutura.
