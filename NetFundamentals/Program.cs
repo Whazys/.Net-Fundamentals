1731c343967f9ce6ae9bd317abfe228eb231a94a
whazys <santos_rafa1973@hotmail.com>
2021-11-21 13:22:55 -0300

    .Net-Fundamentals - DiO

---
+++ NetFundamentals/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int numeroDeVezes = 5;
+for (int i = 0; i < numeroDeVezes; i++)
+{
+  Console.WriteLine($"Bora estudar, Rafa! {i}");
+}
